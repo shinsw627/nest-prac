48015303f4f47eb4fa78060d8e28063f702e9780
shinsw627 <shinsw627@naver.com>
2022-02-13 22:23:55 +0900

    repository 패턴 적용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { UserRequestDto } from './dtos/users.request.dto';
-import { User } from './users.schema';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
